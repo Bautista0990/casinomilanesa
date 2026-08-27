a4f9045feafe794e07d16bb41aba61fe9d01b255
Bautista0990 <bautirosales0207@gmail.com>
2026-08-27 17:19:40 -0300

    Agrego JuegoFavorito al RegistroDto

--- DTOs/Dtos.cs
+++ DTOs/Dtos.cs
@@ -1,5 +1,5 @@
 namespace CasinoMilanesaAPI.DTOs;
 
-public record RegistroDto(string Nombre, string Apellido, string Email, string Password);
+public record RegistroDto(string Nombre, string Apellido, string Email, string Password,string JuegoFavorito);
 public record LoginDto(string Email, string Password);
 public record EditarUsuarioDto(string JuegoFavorito);
